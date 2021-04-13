6d9c40e55278e7a3dda3163f266856733aba2cc2
TGreeneIII <67372744+TGreeneIII@users.noreply.github.com>
2021-04-13 10:53:48 -0400

    Update custom_text_header.js

--- CSS/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ CSS/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -7,10 +7,10 @@
 
 let custom_text_header = `
 <div class="custom-text-header"> <pre>  _______   ____ ________  __  _________
- /_  __/ | / / //_/ ____/    /  |/  / ____/
+ /_  __/ | / / //_/ ____/ /  |/  / ____/
   / / /  |/ / ,&lt; / / __  / /|_/ / __/   
  / / / /|  / /| / /_/ / /  /  / / /___   
-/_/ /_/ |_/_/ |_\\____/(_)_/ /_/_____/   
+/_/ /_/ |_/_/ |_\\____/(_)/ /_/_____/   
 </pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
